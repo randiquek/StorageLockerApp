a70a8f03094dc95694dd0097666f4acbaa10228d
randiquek <randiquek@gmail.com>
2024-12-16 07:18:30 -0800

    final app changes

--- src/StorageLockerApp.java
+++ src/StorageLockerApp.java
@@ -6,9 +6,9 @@ public class StorageLockerApp {
     // Define variables/arrays
     // Establish variable for 10 lockers
     public static int totalAvailableLockers = 10;
-    public static String[] lockerID = {"1", "2", "3"};
-    public static String[] lockerPin = {"0001", "0002", "0003"};
-    public static boolean[] lockerAvailable = {true, true, true};
+    public static String[] lockerID = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    public static String[] lockerPin = {"0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010"};
+    public static boolean[] lockerAvailable = {true, true, true, true, true, true, true, true, true, true};
     // Set up Scanner for input
 
     public static void main(String[] args) {
@@ -31,33 +31,6 @@ public class StorageLockerApp {
 
             }
         }
-    /*
-
-    1. Display Main Menu
-        a. Rent
-        b. Access
-        c. Release
-        d. Exit
-    2. User Rents a Locker
-        a. Find available locker
-        b. Provide pin # (make random once everything works)
-        c. Print a thank-you with locker# and pin
-
-    3. User Accesses a Locker
-        a. Enter locker#
-        b. If locker is rented, ask for pin.
-        c. Enter locker pin#
-        d. If pin is correct, open locker and print success msg
-        e. If pin is incorrect ask for pin # again
-        f. Display the main menu
-    4. User Releases Locker
-        a. Access the locker
-        b. Confirm user is sure
-        c. If cannot confirm, return to start menu
-    5. User Exits
-        a. Exit application
-     */
-
 
     }
 
@@ -69,16 +42,7 @@ public class StorageLockerApp {
             }
          System.out.println("2. Access a Locker \n3. Release a Locker \n--- \nAny other key to exit");
      }
-        /*
-        - What would you like to do next?
-        - If locker is available, print: 1. Rent a Locker
-        - 2. Access a Locker
-        - 3. Release a Locker
-        ---
-        - Any other key to Exit
 
-
-         */
     // getChoice()
     public static int getChoice() {
         Scanner console = new Scanner(System.in);
@@ -115,12 +79,12 @@ public class StorageLockerApp {
     }
     public static int releaseLocker() {
         int releaseLockerIndex = getLockerNumber();
-        if(getLockerNumber() == -1) {
+        if(releaseLockerIndex != -1) {
             for(int i = 0; i < lockerAvailable.length; i++) {
-                lockerAvailable[releaseLockerIndex - 1] = true;
+                System.out.println("Locker released and is available to rent");
+                lockerAvailable[releaseLockerIndex] = true;
                 break;
             }
-            System.out.println("Locker released and is available to rent");
         }
         return releaseLockerIndex;
     }
@@ -151,8 +115,8 @@ public class StorageLockerApp {
         }
         return noLockerMatch;
     }
-    public static void openLocker() {
-        System.out.println("Success! ");
-    }
+//    public static void openLocker() {
+//        System.out.println("Success! ");
+//    }
 
 }
